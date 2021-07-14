3b7850915356c91eb16331a4d9095a5bf98131ed
Alex Smith <1763@holbertonschool.com>
2021-07-14 19:33:40 +0000

    changed import placement for checker

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -2,11 +2,11 @@
 """
 module for task 13
 """
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
     """
     concantenates two numpy matrix along axis
     """
-    import numpy as np
     return np.concatenate((mat1, mat2), axis)
--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -2,10 +2,11 @@
 """
 module for task 14
 """
+import numpy as np
 
 
 def np_matmul(mat1, mat2):
     """
     performs matrix multiplication for numpy
     """
-    return mat1 @ mat2
+    return np.matmul(mat1, mat2)
