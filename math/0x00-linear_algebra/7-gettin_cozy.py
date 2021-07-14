3f4b709c4d42afebf390d27841b53b2b5bccd86a
Alex Smith <1763@holbertonschool.com>
2021-07-14 18:54:22 +0000

    added case for error

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,18 +9,21 @@ def cat_matrices2D(mat1, mat2, axis=0):
     concatenates two matrixs
     """
     result = []
-    if axis == 0:
-        for i in mat1:
-            result.append(i.copy())
-        for i in mat2:
-            result.append(i.copy())
-        return result
-    elif axis == 1:
-        for x in range(0, len(mat1)):
-            result.append([])
-            for y in mat1[x]:
-                result[x].append(y)
-        for x in range(0, len(result)):
-            for y in mat2[x]:
-                result[x].append(y)
-        return result
+    try:
+        if axis == 0:
+            for i in mat1:
+                result.append(i.copy())
+            for i in mat2:
+                result.append(i.copy())
+            return result
+        elif axis == 1:
+            for x in range(0, len(mat1)):
+                result.append([])
+                for y in mat1[x]:
+                    result[x].append(y)
+            for x in range(0, len(result)):
+                for y in mat2[x]:
+                    result[x].append(y)
+            return result
+    except IndexError:
+        return None
