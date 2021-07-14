ece61efb7746025c6d4293c499f5ea6d6a14fe8c
Alex Smith <1763@holbertonschool.com>
2021-07-14 19:36:40 +0000

    added case for None

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,21 +9,20 @@ def cat_matrices2D(mat1, mat2, axis=0):
     concatenates two matrixs
     """
     result = []
-    try:
-        if axis == 0:
-            for i in mat1:
-                result.append(i.copy())
-            for i in mat2:
-                result.append(i.copy())
-            return result
-        elif axis == 1:
-            for x in range(0, len(mat1)):
-                result.append([])
-                for y in mat1[x]:
-                    result[x].append(y)
-            for x in range(0, len(result)):
-                for y in mat2[x]:
-                    result[x].append(y)
-            return result
-    except:
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        for i in mat1:
+            result.append(i.copy())
+        for i in mat2:
+            result.append(i.copy())
+        return result
+    elif axis == 1 and len(mat1) == len(mat2):
+        for x in range(0, len(mat1)):
+            result.append([])
+            for y in mat1[x]:
+                result[x].append(y)
+        for x in range(0, len(result)):
+            for y in mat2[x]:
+                result[x].append(y)
+        return result
+    else:
         return None
