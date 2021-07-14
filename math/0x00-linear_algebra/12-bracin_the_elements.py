933aaf67a125bb4ae80d355a020dc990aab0f41b
Alex Smith <1763@holbertonschool.com>
2021-07-14 18:49:29 +0000

    fixed typo

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -8,4 +8,4 @@ def np_elementwise(mat1, mat2):
     """
     performs element-wise add, subtract, multiply, and divide
     """
-    return mat1 + mat1, mat1 - mat2, mat1 * mat2, mat1 / mat2
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
