e4d6e274753f105cec37f6839a2f2741e41905e6
Alex Smith <1763@holbertonschool.com>
2021-07-21 18:26:58 +0000

    checking fix

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -9,7 +9,7 @@ def poly_integral(poly, C=0):
     calculates integral of polynomial
     """
     result = []
-    if type(poly) is not list or type(C) is not int:
+    if type(poly) is not list or type(C) is not int or len(poly) == 0:
         return None
     if len(poly) > 0:
         result.append(C)
@@ -21,6 +21,6 @@ def poly_integral(poly, C=0):
             if float.is_integer(num):
                 num = int(num)
             result.append(num)
-    if len(result) == 2 and result[0] == 0 and result[1] == 0:
+    if len(result) == 2 and (result[0] == 0 or result[1] == 0):
         result.remove(0)
     return result
