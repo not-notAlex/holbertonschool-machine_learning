b6f0ea0b1a7d01f6ec11967e8dbeb8c69525e25c
Alex Smith <1763@holbertonschool.com>
2021-07-21 18:34:58 +0000

    small fix

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -13,6 +13,8 @@ def poly_derivative(poly):
         return None
     for i in range(1, len(poly)):
         result.append(i * poly[i])
-    if len(result) == 0:
+    if len(result) == 1:
         result.append(0)
+    if len(result) == 0:
+        return None
     return result
