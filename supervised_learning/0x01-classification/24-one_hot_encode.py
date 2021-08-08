b1a6dbcadf5532a31c6ccedf5a940c1e53b2ef32
Alex Smith <1763@holbertonschool.com>
2021-08-08 21:30:24 +0000

    fixed edge cases

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -10,4 +10,8 @@ def one_hot_encode(Y, classes):
     """
     encodes labels vector into one-hot matrix
     """
+    if type(Y) is not np.ndarray:
+        return None
+    if type(classes) is not int or classes < 2:
+        return None
     return np.eye(classes)[Y].T
