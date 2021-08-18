b3a761da125f7a76a58a5fde635dbf11b4814d8f
Alex Smith <1763@holbertonschool.com>
2021-08-18 19:54:45 +0000

    answer with bad formatting

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -3,7 +3,21 @@
 module for task 0
 """
 
-import numpy as np
 import tensorflow as tf
 
 
+def create_batch_norm_layer(prev, n, activation):
+    """
+    creates batch normalization layer for network
+    """
+    W = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    model = tf.layers.Dense(units=n, name="layer", kernel_initializer=W)
+    X = model(prev)
+    mean, variance = tf.nn.moments(X, [0])
+    return activation(
+        tf.nn.batch_normalization(
+            X, mean, variance, offset=tf.Variable(
+                tf.constant(
+                    0.0, shape=[n]), trainable=True),
+            scale=tf.Variable(tf.constant(1.0, shape=[n]),
+                              trainable=True), variance_epsilon=1e-8))
