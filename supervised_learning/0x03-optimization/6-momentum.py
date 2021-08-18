18ac14916b5c2980826bc5570b3d7be485b18c4d
Alex Smith <1763@holbertonschool.com>
2021-08-18 18:43:58 +0000

    answer

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -3,7 +3,11 @@
 module for task 0
 """
 
-import numpy as np
 import tensorflow as tf
 
 
+def create_momentum_op(loss, alpha, beta1):
+    """
+    creates training op using gradient descent w/ momentum algorithm
+    """
+    return tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
