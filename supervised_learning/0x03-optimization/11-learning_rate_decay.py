4d569857979e7f7bcac8c9009691028770d632ae
Alex Smith <1763@holbertonschool.com>
2021-08-18 19:37:50 +0000

    answer

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -4,6 +4,10 @@ module for task 0
 """
 
 import numpy as np
-import tensorflow as tf
 
 
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    updates learning rate using inverse time decay
+    """
+    return alpha / (1 + decay_rate * np.floor(global_step / decay_step))
