70e65836e3bf2ff4f804ab1262081e63651980fb
Alex Smith <1763@holbertonschool.com>
2021-08-18 19:30:04 +0000

    answer

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -3,7 +3,14 @@
 module for task 0
 """
 
-import numpy as np
-import tensorflow as tf
 
-
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """
+    updates variables using Adam algorithm
+    """
+    vdW = (beta1 * v) + ((1 - beta1) * grad)
+    sdW = (beta2 * s) + ((1 - beta2) * (grad ** 2))
+    vdW_change = vdW / (1 - (beta1 ** t))
+    sdW_change = sdW / (1 - (beta2 ** t))
+    var -= alpha * (vdW_change / (epsilon + (sdW_change ** (1 / 2))))
+    return var, vdW, sdW
