bd5326a74597acc7d5f44b71a73aacd1f3405340
Alex Smith <1763@holbertonschool.com>
2021-08-25 19:29:02 +0000

    answer

--- supervised_learning/0x05-regularization/7-early_stopping.py
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 """
-module for task 0
+module for task 7
 """
 
-import numpy as np
-import tensorflow as tf
 
-
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """
+    determines if you should stop gradient descent early
+    """
+    if opt_cost - cost > threshold:
+        count = 0
+    else:
+        count = count + 1
+    if count == patience:
+        return True, count
+    else:
+        return False, count
