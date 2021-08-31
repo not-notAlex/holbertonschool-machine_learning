f6a175176287b3fe7984a329e473000eea8009c0
Alex Smith <1763@holbertonschool.com>
2021-08-31 07:29:29 +0000

    answer

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 """
-module for task 0
+module for task 6
 """
 
 import tensorflow.keras as K
 
 
+def train_model(network, data, labels,
+                batch_size, epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """
+    trains a model using mini-batch gradient descent
+    """
+    callbacks = []
+    if early_stopping and validation_data:
+        callbacks.append(K.callbacks.EarlyStopping(patience=patience))
+    return network.fit(
+        x=data, y=labels, batch_size=batch_size,
+        epochs=epochs, validation_data=validation_data,
+        callbacks=callbacks, verbose=verbose, shuffle=shuffle)
