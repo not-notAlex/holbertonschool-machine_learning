2271bc191b20fc2f92469ec62b844df7299fbd37
Alex Smith <1763@holbertonschool.com>
2021-08-31 08:09:18 +0000

    answer

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 """
-module for task 0
+module for task 11
 """
 
 import tensorflow.keras as K
 
 
+def save_config(network, filename):
+    """
+    saves network in JSON
+    """
+    output = network.to_json()
+    with open(filename, 'w+') as f:
+        f.write(output)
+    return None
+
+
+def load_config(filename):
+    """
+    loads network from JSON
+    """
+    with open(filename, 'r') as f:
+        inputs = f.read()
+    return K.models.model_from_json(inputs)
