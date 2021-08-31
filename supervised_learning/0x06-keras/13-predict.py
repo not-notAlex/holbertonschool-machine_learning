64bfdeb5928f2c297145e24cf8d2f7ef80657aba
Alex Smith <1763@holbertonschool.com>
2021-08-31 08:14:06 +0000

    answer

--- supervised_learning/0x06-keras/13-predict.py
+++ supervised_learning/0x06-keras/13-predict.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 """
-module for task 0
+module for task 13
 """
 
 import tensorflow.keras as K
 
 
+def predict(network, data, verbose=False):
+    """
+    makes a prediction using network
+    """
+    return network.predict(x=data, verbose=verbose)
