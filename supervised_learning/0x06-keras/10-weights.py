a4ebd65747374f0f376c4eb07e28755551cb86b8
Alex Smith <1763@holbertonschool.com>
2021-08-31 08:04:27 +0000

    answer

--- supervised_learning/0x06-keras/10-weights.py
+++ supervised_learning/0x06-keras/10-weights.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 """
-module for task 0
+module for task 10
 """
 
 import tensorflow.keras as K
 
 
+def save_weights(network, filename, save_format='h5'):
+    """
+    saves weights from network
+    """
+    network.save_weights(filename, save_format=save_format)
+    return None
+
+
+def load_weights(network, filename):
+    """
+    loads weights into network
+    """
+    network.load_weights(filename)
+    return None
