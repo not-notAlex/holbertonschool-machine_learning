5edac7b6e978ead72f3044feb9f17be96c513d58
Alex Smith <1763@holbertonschool.com>
2021-08-31 07:15:31 +0000

    fixed style

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -10,4 +10,6 @@ def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle
     """
     trains a model using mini-batch gradient descent
     """
-    return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=shuffle)
+    return network.fit(
+        x=data, y=labels, batch_size=batch_size,
+        epochs=epochs, verbose=verbose, shuffle=shuffle)
