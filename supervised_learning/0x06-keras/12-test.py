ed1fb484678c422eb22951b54976514d2f76f9a1
Alex Smith <1763@holbertonschool.com>
2021-08-31 08:12:16 +0000

    answer

--- supervised_learning/0x06-keras/12-test.py
+++ supervised_learning/0x06-keras/12-test.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 """
-module for task 0
+module for task 12
 """
 
 import tensorflow.keras as K
 
 
+def test_model(network, data, labels, verbose=True):
+    """
+    tests a neural network
+    """
+    return network.evaluate(x=data, y=labels, verbose=verbose)
