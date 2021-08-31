a6ad144350a261361571d3b1b308c02b35ea9d4d
Alex Smith <1763@holbertonschool.com>
2021-08-31 08:01:13 +0000

    answer

--- supervised_learning/0x06-keras/9-model.py
+++ supervised_learning/0x06-keras/9-model.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 """
-module for task 0
+module for task 9
 """
 
 import tensorflow.keras as K
 
 
+def save_model(network, filename):
+    """
+    saves a model
+    """
+    network.save(filename)
+
+
+def load_model(filename):
+    """
+    loads a model
+    """
+    return K.models.load_model(filename)
