785d61637556898df8224df24f2f889156dfcee7
Alex Smith <1763@holbertonschool.com>
2021-09-08 20:38:54 +0000

    testing answer

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -10,17 +10,17 @@ def lenet5(x, y):
     """
     builds a modified version of the LeNet-5
     """
-    ki = (tf.contrib.layers.variance_scaling_initializer())
-    L1 = tf.layers.Conv2D(6, 5, padding='same', activation='relu', kernel_initializer=ki)(x)
-    L2 = tf.layers.MaxPooling2D(2, 2)(L1)
-    L3 = tf.layers.Conv2D(16, 5, padding='valid', activation='relu', kernel_initializer=ki)(L2)
-    L4 = tf.layers.MaxPooling2D(2, 2)(L3)
-    L5 = tf.layers.Flatten()(L4)
-    L6 = tf.layers.Dense(120, activation='relu', kernel_initializer=ki)(L5)
-    L7 = tf.layers.Dense(84, activation='relu', kernel_initializer=ki)(L6)
-    out = tf.layers.Dense(10, activation='softmax', kernel_initializer=ki)(L7)
+    initializer = (tf.contrib.layers.variance_scaling_initializer())
+    out = tf.layers.Conv2D(6, 5, padding='same', activation='relu', kernel_initializer=initializer)(x)
+    out = tf.layers.MaxPooling2D(2, 2)(out)
+    out = tf.layers.Conv2D(16, 5, padding='valid', activation='relu', kernel_initializer=initializer)(out)
+    out = tf.layers.MaxPooling2D(2, 2)(out)
+    out = tf.layers.Flatten()(out)
+    out = tf.layers.Dense(120, activation='relu', kernel_initializer=initializer)(out)
+    out = tf.layers.Dense(84, activation='relu', kernel_initializer=initializer)(out)
+    out = tf.layers.Dense(10, activation='softmax', kernel_initializer=initializer)(out)
     loss = tf.losses.softmax_cross_entropy(y, out)
     train = tf.train.AdamOptimizer().minimize(loss)
-    equality = tf.equal(tf.argmax(y, axis=1), tf.argmax(out, axis=1))
-    accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
+    equal = tf.equal(tf.argmax(y, 1), tf.argmax(out, 1))
+    accuracy = tf.reduce_mean(tf.cast(equal, tf.float32))
     return out, train, loss, accuracy
