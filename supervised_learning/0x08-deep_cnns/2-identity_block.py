23ce719d3d564d2873252fcee3e4b42eac705fdb
Alex Smith <1763@holbertonschool.com>
2021-09-15 19:50:12 +0000

    fixed styling

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -14,7 +14,8 @@ def identity_block(A_prev, filters):
     L1 = K.layers.Conv2D(LF1, 1, kernel_initializer='he_normal')(A_prev)
     L2 = K.layers.BatchNormalization()(L1)
     L3 = K.layers.Activation('relu')(L2)
-    L4 = K.layers.Conv2D(LF4, 3, padding='same', kernel_initializer='he_normal')(L3)
+    L4 = K.layers.Conv2D(
+        LF4, 3, padding='same', kernel_initializer='he_normal')(L3)
     L5 = K.layers.BatchNormalization()(L4)
     L6 = K.layers.Activation('relu')(L5)
     L7 = K.layers.Conv2D(LF7, 1, kernel_initializer='he_normal')(L6)
