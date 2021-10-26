8c824a21749068ea4b60a57fb145e676e2c8b448
Alex Smith <1763@holbertonschool.com>
2021-10-26 16:47:56 +0000

    answer

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -6,3 +6,16 @@ module for task 0
 import numpy as np
 
 
+def pca(X, var=0.95):
+    """
+    performs PCA on a dataset
+    """
+    u, s, v = np.linalg.svd(X)
+    a = np.cumsum(s)
+    dim = []
+    l = s.shape[0]
+    for i in range(l):
+        if ((a[i]) / a[-1]) >= var:
+            dim.append(i)
+    r = dim[0] + 1
+    return v.T[:, :r]
--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """
-module for task 0
+module for task 1
 """
 
 import numpy as np
 
 
+def pca(X, ndim):
+    """
+    performs PCA on a dataset
+    """
+    mean = np.mean(X, axis=0, keepdims=True)
+    a = X - mean
+    u, s, v = np.linalg.svd(a)
+    w = v.T[:, :ndim]
+    return np.matmul(a, w)
