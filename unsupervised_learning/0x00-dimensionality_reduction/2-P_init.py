1c99f4b07440d8b8d5c87a7936b23f058c4a295d
Alex Smith <1763@holbertonschool.com>
2021-10-27 18:22:06 +0000

    fix

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -10,10 +10,11 @@ def P_init(X, perplexity):
     """
     initializes variables required to calculate t-SNE
     """
-    n = X.shape[0]
-    m = np.matmul(X, -X.T)
-    D = np.add(np.add(-2 * np.dot(X, X.T), np.sum(np.square(X), 1)).T, np.sum(np.square(X), 1))
-    P = np.zeros((X.shape[0], X.shape[0]))
+    D = np.add(np.add(-2 * np.dot(X, X.T), np.sum(
+        np.square(X), 1)).T, np.sum(np.square(X), 1))
+    np.fill_diagonal(D, 0.)
     betas = np.ones((X.shape[0], 1))
+    P = np.zeros((X.shape[0], X.shape[0]))
+    sumP = sum(P)
     H = np.log2(perplexity)
     return D, P, betas, H
