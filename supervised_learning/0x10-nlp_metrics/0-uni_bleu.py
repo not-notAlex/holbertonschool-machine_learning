c631b5adae1f5cff49a9e4f4f7ccaa80d4d04334
Alex Smith <1763@holbertonschool.com>
2021-12-19 20:34:55 +0000

    one answer

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -6,3 +6,19 @@ module for task 0
 import numpy as np
 
 
+def uni_bleu(references, sentence):
+    """
+    calculates unigram BLEU score for sentence
+    """
+    sentence_length = len(sentence)
+    references_length = []
+    words = {}
+    for translation in references:
+        references_length.append(len(translation))
+        for word in translation:
+            if word in sentence and word not in words.keys():
+                words[word] = 1
+    total = sum(words.values())
+    index = np.argmin([abs(len(i) - sentence_length) for i in references])
+    BLEU = np.exp(1 - float(len(references[index])) / float(sentence_length))
+    return BLEU * np.exp(np.log(total / sentence_length))
