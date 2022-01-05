141ec69a98f6d9fbfd27efe4e1b25d46180a24f0
Alex Smith <1763@holbertonschool.com>
2022-01-05 23:27:29 +0000

    small change

--- supervised_learning/0x11-attention/8-transformer_decoder_block.py
+++ supervised_learning/0x11-attention/8-transformer_decoder_block.py
@@ -33,11 +33,12 @@ class DecoderBlock(tf.keras.layers.Layer):
         """
         attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)
         attn1 = self.dropout1(attn1, training=training)
-        attn2, attn_weights_block2 = self.mha2(
-            out1, encoder_output, encoder_output, padding_mask)
+        out1 = self.layernorm1(attn1 + x)
+        attn2, attn_weights_block2 = self.mha2(out1, encoder_output, encoder_output, padding_mask)
         attn2 = self.dropout2(attn2, training=training)
-        out2 = self.layernorm2(attn2 + self.layernorm1(attn1 + x))
+        out2 = self.layernorm2(attn2 + out1)
         ffn = tf.keras.Sequential([self.dense_hidden, self.dense_output])
         ffn_output = ffn(out2)
         ffn_output = self.dropout3(ffn_output, training=training)
-        return self.layernorm3(ffn_output + out2)
+        out3 = self.layernorm3(ffn_output + out2)
+        return out3
