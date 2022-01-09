dd65b87eaca98c0fd140933ceee3976430881bc9
Alex Smith <1763@holbertonschool.com>
2022-01-09 22:31:48 +0000

    one answer

--- supervised_learning/0x12-transformer_apps/0-dataset.py
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -7,3 +7,29 @@ import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
 
 
+class Dataset:
+    """
+    dataset class
+    """
+    def __init__(self):
+        """
+        class constructor
+        """
+        self.data_train = tfds.load(
+            'ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation', as_supervised=True)
+        tokenizer_pt, tokenizer_en = self.tokenize_dataset(self.data_train)
+        self.tokenizer_pt = tokenizer_pt
+        self.tokenizer_en = tokenizer_en
+
+    def tokenize_dataset(self, data):
+        """
+        creates sub-word tokenizers for dataset
+        """
+        subword = tfds.features.text.SubwordTextEncoder.build_from_corpus
+        tokenizer_en = subword(
+            (en.numpy() for pt, en in data), target_vocab_size=2**15)
+        tokenizer_pt = subword(
+            (pt.numpy() for pt, en in data), target_vocab_size=2**15)
+        return tokenizer_pt, tokenizer_en
